1ca3c14b971e44917fad149aa29c88cf7e296900
arsenalkingsley <stepupitsreal@gmail.com>
2022-12-14 08:02:45 -0800

    anothercommit

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,8 @@
+
+
+/**
+ * _atoi - converts the first set of digits in a string to an integer,
+ * taking into account the sign of the number
+ * @s: string to be converted
+ * Return: int containing the converted number
+ */
